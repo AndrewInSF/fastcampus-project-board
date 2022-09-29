c4e0855690f9a23c6fe30a3124cc0cc9d26bc31f
AndrewInSF <commercial209@gmail.com>
2022-09-30 03:54:11 +0900

    #16 유저 기능 추가 및 로그인 컨트롤러 테스트 + 시큐리티 설정 반영
    스프링 시큐리티가 자동 생성해준 `login`컨트롤러 테스트 그리고 시큐리티 설정을 부를수 있도록 `@import` 추가

--- src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/UserAccountRepository.java
@@ -1,2 +1,7 @@
-package com.fastcampus.projectboard.repository;public interface UserAccountRepository {
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 }
